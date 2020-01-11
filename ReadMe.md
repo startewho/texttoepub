f738804fe79775515bd86ceaa64d299fe817e700
startewho <hoolooday@gmail.com>
2020-01-11 16:16:40 +0800

    处理第一章之前的文本

--- ReadMe.md
+++ ReadMe.md
@@ -2,7 +2,15 @@
 
 A text to epub creater.
 
+## Furter
+
+* muti Encoding utf8/gbk
+* 
+
 
 ## Todo
+* startup args
+* docs
+* a lib
 
 
--- src/epub.rs
+++ src/epub.rs
@@ -19,6 +19,18 @@ pub fn gen_epub<'a>(book: meta::Book<'a>) -> Result<()> {
     for m in book.get_metas().iter() {
         buider.metadata(&m.key, &m.value)?;
     }
+    let len= book.get_chapter().len();
+    if len>0 {
+        let fir_c=&book.get_chapter()[0];
+        let fir_start=fir_c.get_start();
+        if fir_start>0 {
+            let capter = gen_chapter(&(book.get_source()[0..fir_start]));
+            let econtent = EpubContent::new(format!("chapter{:?}.html", 0), capter.as_bytes())
+                .title("前言");
+            buider.add_content(econtent)?;
+        }
+    }   
+
     let mut index = 1;
     for c in book.get_chapter().iter() {
         let start = c.get_start();
@@ -28,16 +40,15 @@ pub fn gen_epub<'a>(book: meta::Book<'a>) -> Result<()> {
         } else {
             next_c = book.get_chapter()[index].get_start();
         }
-
-        let cpater = gen_chapter(&(book.get_source()[start..next_c]));
-        let econtent = EpubContent::new(format!("chapter{:?}.html", index), cpater.as_bytes())
+        let capter = gen_chapter(&(book.get_source()[start..next_c]));
+        let econtent = EpubContent::new(format!("chapter{:?}.html", index), capter.as_bytes())
             .title(c.get_name());
         buider.add_content(econtent)?;
         index += 1;
     }
     // Set the stylesheet (create a "stylesheet.css" file in EPUB that is used by some generated files)
     buider.stylesheet(dummy_css.as_bytes())?;
-
+    
     // Generate a toc inside of the document, that will be part of the linear structure.
     buider.inline_toc().generate(&mut buffer)?;
     Ok(())
